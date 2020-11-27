521aec4790fa26334a5d168bad6b84c343b4d59b
dark <74014571+darkhacker707@users.noreply.github.com>
2020-11-27 15:00:48 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+clear
+sudo apt update
+sudo apt upgrade -y
+sudo apt install sqlmap
+sudo apt install nmap
+sudp apt install python
+sudo apt install python2
+sudo apt install python3
+sudo apt install php
+echo
+echo   '###     ### #####       #######    ###########       #######    ####       ####'
+echo   '###     #####  ###     ######      ###########       #######    ####       ####'
+echo   '###     ###     ###    #####           ###               ###    ####       ####'
+echo   '###     ###     ###     #####          ###        ##########    ####       ####'
+echo   '###     ###     ###       #####        ###       ###########    ####       ####'
+echo   '###     ###     ###        #####       ###       ##       ##    ####       ####'
+echo   '###     ###     ###        ######      ###       ##       ##    ####       ####'
+echo   '###     ###     ###       ######       ###       ###########    #######    #######'
+echo   '###     ###     ###     #######        ###        ##########    #######    #######'
+echo
